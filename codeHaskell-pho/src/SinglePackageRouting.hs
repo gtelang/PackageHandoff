69c083f400521906485650c0b53f9aad298a9f43
Gaurish Telang <gaurish108@gmail.com>
2016-09-24 19:40:07 -0400

    Added some lines to Single Package Handoff

--- codeHaskell-pho/src/SinglePackageRouting.hs
+++ codeHaskell-pho/src/SinglePackageRouting.hs
@@ -1,23 +1,25 @@
 
 {-# LANGUAGE UnicodeSyntax #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
+  {-# LANGUAGE NoMonomorphismRestriction #-}
 
-{- | This module contains scheduling algorithms and data-structures for 
-     routing a single package from point A to point B, where the carrier-
-     robots have varying velocity and possibly limited fuel.
--}
+  {- | This module contains scheduling algorithms and data-structures for 
+       routing a single package from point A to point B, where the carrier-
+       robots have varying velocity and possibly limited fuel.
+  -}
 
-module SinglePackageRouting where
+  module SinglePackageRouting where
 
-import Diagrams.Prelude
+  import Diagrams.Prelude
 
-import qualified Data.List as List
-import qualified Data.Map  as Map
-import qualified Data.Set  as Set
-import qualified Data.Function as Function
-import Control.Monad
-import Data.Monoid 
+  import qualified Data.List as List
+  import qualified Data.Map  as Map
+  import qualified Data.Set  as Set
+  import qualified Data.Function as Function
+  import Control.Monad
+  import Data.Monoid 
 
-import Data.Colour.Palette.BrewerSet
-import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game
+  import Data.Colour.Palette.BrewerSet
+  import Graphics.Gloss
+  import Graphics.Gloss.Interface.Pure.Game
+
+Made some edits here
