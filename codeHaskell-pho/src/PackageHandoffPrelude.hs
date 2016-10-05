36626e6e373cdc7803e2ce3718ebceba4a57d4de
Gaurish Telang <gaurish108@gmail.com>
2016-10-05 02:10:59 -0400

    Finalized the design of Basic Data-types in PackageHandOffPrelude.hs

--- codeHaskell-pho/src/PackageHandoffPrelude.hs
+++ codeHaskell-pho/src/PackageHandoffPrelude.hs
@@ -10,21 +10,25 @@ import Diagrams.Prelude
 import qualified Data.Map as Map
 
 type Speed       = Double  -- | ∈ [0,1]
-type Fuel        = Double  -- | ≥ 0
-type Time        = Double  -- | ≥ 0
+type Fuel        = Double  -- | ∈ [0,∞] 
+type RobotIdx    = Int     -- | ≥ 0
 type PackageIdx  = Int     -- | ≥ 0 
-type RobotIdx    = Int     -- | >= 0
 type PackageList = [PackageIdx]
 
 data Package = Package { source :: Point V2 Double,
                          target :: Point V2 Double 
                        } deriving (Show)
 
-data Robot = Robot { initPosition :: Point V2 Double, 
-                     speed        :: Speed          ,
-                     fuel         :: Fuel           ,       
-                     schedule     :: Trajectory  
-                   } deriving (Show)
+-- TODO: Should we parametrize the Robot data-type depending on what king of points, it starts at? 
+-- i.e integer grid points or just the R^2 plane.
+data Robot = Robot { idx          :: RobotIdx,
+                     initPosition :: Point V2 Double 
+                   } deriving (Show) 
+ 
+-- We define Robot Equality based on their indices. 
+-- since two Robots are allowed to start at the same position. 
+instance Eq Robot where 
+   Robot idx1 _ == Robot idx2 _ = (idx1 == idx2)
 
 {- | World consists of only robots and packages. -}
 data World = World { robots      :: [ Robot  ] ,    
@@ -32,17 +36,18 @@ data World = World { robots      :: [ Robot  ] ,
                    } deriving (Show) 
              
 {- | Load carried across a link of the trajectory. -} 
-data Load =  -- | Single package load, possibly give to RobotIdx inside Maybe functor  
-             SinglePackage      (   Package     , Maybe RobotIdx )    
-             -- | Multi-package load, possibly give each to RobotIdx inside Maybe functor 
-           | MultiplePackage    [ ( PackageList   , Maybe RobotIdx ) ]  
-             -- | Zero Load, nothing to give!
+data Load =  -- Single package load, possibly give to the Robot inside Maybe functor  
+             SinglePackage      (   Package     , Maybe Robot )    
+             -- Multi-package load, possibly give each to the Robot inside Maybe functor 
+           | MultiPackage       [ ( PackageList , Maybe Robot ) ]  
+             -- Zero Load, nothing to give!
            | Nil                                                      
              deriving (Show)
 
  {- | A single link of the trajectory -}
-data Link = Link { segment :: ()  , 
-                   load    :: Load
+data Link = Link { head            :: Point V2 Double, 
+                   load            :: Load, 
+                   waitTimeAtHead  :: Double
                  } deriving (Show)
 
 {- | Schedule for a single robot.-} 
