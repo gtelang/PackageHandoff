3f933a0c33650aba09f589f038ea5268cdfc2234
Gaurish Telang <gaurish108@gmail.com>
2016-10-05 11:45:54 -0400

    Parametrized the data-types depending on whether points are constrained to Z2 or R2

--- codeHaskell-pho/src/PackageHandoffPrelude.hs
+++ codeHaskell-pho/src/PackageHandoffPrelude.hs
@@ -11,49 +11,47 @@ import qualified Data.Map as Map
 
 type Speed       = Double  -- | ∈ [0,1]
 type Fuel        = Double  -- | ∈ [0,∞] 
-type RobotIdx    = Int     -- | ≥ 0
-type PackageIdx  = Int     -- | ≥ 0 
-type PackageList = [PackageIdx]
+type Robots a    = [Robot a]
+type Packages a  = [Package a]
 
-data Package = Package { source :: Point V2 Double,
-                         target :: Point V2 Double 
-                       } deriving (Show)
+data Package a = Package { source :: Point V2 a,
+                           target :: Point V2 a 
+                         } deriving (Show)
 
--- TODO: Should we parametrize the Robot data-type depending on what king of points, it starts at? 
--- i.e integer grid points or just the R^2 plane.
-data Robot = Robot { idx          :: RobotIdx,
-                     initPosition :: Point V2 Double 
+data Robot a = Robot { idx          :: Int,
+                       initPosition :: Point V2 a
                    } deriving (Show) 
  
 -- We define Robot Equality based on their indices. 
 -- since two Robots are allowed to start at the same position. 
-instance Eq Robot where 
+instance Eq (Robot a) where 
    Robot idx1 _ == Robot idx2 _ = (idx1 == idx2)
 
 {- | World consists of only robots and packages. -}
-data World = World { robots      :: [ Robot  ] ,    
-                     packages    :: [ Package]
-                   } deriving (Show) 
+data World a = World { robots   :: Robots a, 
+                       packages :: Packages a
+                      } deriving (Show) 
              
-{- | Load carried across a link of the trajectory. -} 
-data Load =  -- Single package load, possibly give to the Robot inside Maybe functor  
-             SinglePackage      (   Package     , Maybe Robot )    
+{- | Load carried across a link of the trajectory along with a description of 
+ the exchanges which take place at the link's head. -} 
+data Load a =  -- Single package load, possibly give to the Robot inside Maybe functor  
+             SPack (Package a, Maybe (Robot a))    
              -- Multi-package load, possibly give each to the Robot inside Maybe functor 
-           | MultiPackage       [ ( PackageList , Maybe Robot ) ]  
-             -- Zero Load, nothing to give!
+           | MPack [(Packages a, Maybe (Robot a))]  
+             -- Zero Load, nothing to give to anybody!
            | Nil                                                      
              deriving (Show)
 
  {- | A single link of the trajectory -}
-data Link = Link { head            :: Point V2 Double, 
-                   load            :: Load, 
-                   waitTimeAtHead  :: Double
-                 } deriving (Show)
+data Link a = Link { head :: Point V2 a, 
+                     load :: Load a, 
+                     waitTimeAtHead  :: Double
+                    } deriving (Show)
 
 {- | Schedule for a single robot.-} 
-data Trajectory  = Trajectory [ Link ]  
+data Trajectory a = Trajectory [ Link a ]  
                     deriving (Show)
 
 {- | Schedule for a collection of robots. In one-one 
       correspondence with the robots list of the World -}
-type Schedule    = [ Trajectory ]
+type Schedule a  = [ Trajectory a ]
